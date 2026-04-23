ea0db5a3dff8d28423073cfb919a0b175776d37a
N3agu <andreei1465@yahoo.com>
2026-04-23 21:48:02 +0000

    Add header files from old labs

---
+++ Homework1/Source_Queue_circular.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdexcept>
+
+#ifndef IMPLEMENTATION_SOURCE_QUEUE_CIRCULAR_H
+#define IMPLEMENTATION_SOURCE_QUEUE_CIRCULAR_H
+
+#define NMAX 5
+template<typename T> class Queue_Circular {
+private:
+    T queueArray[NMAX];
+    int head, tail, size;
+public:
+    void enqueue(T x) {
+        if (size == NMAX) {
+            std::cerr << "Queue is full!" << std::endl;
+            return;
+        }
+        queueArray[tail] = x;
+        tail = (tail + 1) % NMAX;
+        size++;
+    }
+
+    T dequeue() {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty!" << std::endl;
+            return T();
+        }
+        T x = queueArray[head];
+        head = (head + 1) % NMAX;
+        size--;
+        return x;
+    }
+    T peek() {
+        if (isEmpty()) {
+            std::cerr << "Queue is empty!" << std::endl;
+            return T();
+        }
+        return queueArray[head];
+    }
+
+    bool isEmpty() {
+        return (size == 0);
+    }
+
+    Queue_Circular() {
+        head = tail = size = 0;
+    }
+};
+
+
+
+#endif //IMPLEMENTATION_SOURCE_QUEUE_CIRCULAR_H
---
+++ Homework1/Source_Stack.h
@@ -0,0 +1,52 @@
+#ifndef SOURCE_STACK_H
+#define SOURCE_STACK_H
+
+#include <iostream>
+#define NMAX 50
+using namespace std;
+
+template <typename T>
+class Source_Stack {
+private:
+    T stackArray[NMAX];
+    int topLevel;
+public:
+    Source_Stack(): topLevel(-1){}
+    ~Source_Stack(){}
+
+    bool isEmpty() {
+        return topLevel == -1;
+    }
+
+
+    void push(T item) {
+        if (topLevel==NMAX-1) {
+            cout<<"Stack is overflow"<<endl;
+            return;
+        }
+        stackArray[++topLevel] = item;
+    }
+
+    T pop() {
+        if(isEmpty()) {
+            cout<<"Stack is empty"<<endl;
+            return T();
+        }
+        return stackArray[topLevel--];
+    }
+
+    T peek() {
+        if(isEmpty()) {
+            cout<<"Stack is empty"<<endl;
+            return T();
+        }
+        return stackArray[topLevel];
+    }
+
+
+
+
+};
+
+
+#endif //SOURCE_STACK_H
